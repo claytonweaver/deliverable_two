da895fd3576a7773ffe62f92e065342ad2e6c3c9
claytonweaver <cweaver@cbctrenton.com>
2019-11-28 23:58:57 -0500

    first change

---
+++ Deliverable_Two.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("Enter your message you would like to be encoded. ");
+        var input = Console.ReadLine().ToUpper();
+        var checksum = 0;
+        var message = "Your encoded message is ";
+        var asciiBytes = Encoding.ASCII.GetBytes(input);
+
+        foreach (var b in asciiBytes)
+        {
+            var c = b - 64;
+            var eachNumber = $"{b - 64}- ";
+            message += eachNumber;
+            checksum += c;
+        }
+        Console.WriteLine(message);
+        Console.WriteLine("Message checksum is " + checksum);
+    }
+}
